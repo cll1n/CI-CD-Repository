6decc61cc91bcd97daebb61c5742d6eb87b09b1e
Collin Forslund <collin@Air-von-Collin.fritz.box>
2023-06-25 21:07:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function Calculator() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleNumber1Change = (e) => {
+    setNumber1(e.target.value);
+  };
+
+  const handleNumber2Change = (e) => {
+    setNumber2(e.target.value);
+  };
+
+  const handleCalculate = () => {
+    const sum = parseFloat(number1) + parseFloat(number2);
+    setResult(sum);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-8">Calculator</h1>
+      <div className="flex items-center">
+        <input
+          className="w-24 p-2 text-center border rounded-l"
+          type="number"
+          placeholder="number 1"
+          value={number1}
+          onChange={handleNumber1Change}
+        />
+        <span className="text-3xl mx-4">+</span>
+        <input
+          className="w-24 p-2 text-center border rounded-r"
+          type="number"
+          value={number2}
+          placeholder="number 2"
+          onChange={handleNumber2Change}
+        />
+        <span className="text-3xl mx-4">=</span>
+        <span className="text-4xl">{result}</span>
+      </div>
+      <button
+        className="mt-8 bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-6 rounded"
+        onClick={handleCalculate}
+      >
+        Calculate
+      </button>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Calculator", () => {
+  it("calculates the sum correctly", () => {
+    render(<App />);
+
+    const number1Input = screen.getByPlaceholderText("number 1");
+    const number2Input = screen.getByPlaceholderText("number 2");
+    const calculateButton = screen.getByText("Calculate");
+
+    // Enter test values
+    fireEvent.change(number1Input, { target: { value: "2" } });
+    fireEvent.change(number2Input, { target: { value: "3" } });
+
+    // Click Calculate button
+    fireEvent.click(calculateButton);
+
+    // Check if the result is displayed correctly
+    const resultLabel = screen.getByText("5");
+    expect(resultLabel).toBeInTheDocument();
+  });
 });
